19d16e07c415347f56997f098b7eea58be921d37
rtocastro <ricktorres@live.com>
2024-02-19 11:38:37 -0800

    initial commit

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Header from './components/Header'
+import About from './pages/About'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <About />
+     
+    </>
+  )
+}
+
+export default App
---
+++ myportfolio/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <>
+        <h1>this is my header</h1>
+        </>
+    )
+}
